e47fded027affe943480e8b2cb34d7f4640a964f
eMobilis <echotosilas@gmail.com>
2024-02-13 09:43:02 +0300

    python sector

---
+++ object oriented programme.py
@@ -0,0 +1,25 @@
+class People:
+    def __init__(self,name, age, nationality):
+        self.name = name
+        self.age = age
+        self.nationality = nationality
+
+person1 = People("Hannah", 19, "Norwegian")
+
+print(person1.name, person1.age, person1.nationality)
+print(f"Hi, my name is {person1.name} and I am {person1.age} years old and I am a {person1.nationality}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
